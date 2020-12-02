1f3168de7d076fb5a3e099fbef6ec9dd2d90ab94
Kelsey Freese <kmfreese@protonmail.com>
2020-12-02 11:57:01 -0600

    Day 2

--- src/Days/Day02.hs
+++ src/Days/Day02.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Days.Day02 (runDay) where
 
 {- ORMOLU_DISABLE -}
@@ -11,6 +13,8 @@ import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
+import qualified Data.Text as T
+
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
@@ -21,19 +25,55 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many' $ do
+  min <- decimal
+  char '-'
+  max <- decimal
+  skipSpace
+  letter <- anyChar
+  char ':'
+  skipSpace
+  password <- takeTill isEndOfLine 
+  endOfLine
+  return $ Password letter min max password
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Password]
 
-type OutputA = Void
+data Password = Password { letter :: Char
+                         , min :: Int
+                         , max :: Int
+                         , password :: T.Text
+                         }
+                deriving Show
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = foldr ((+) . boolToInt . isValidA) 0
+
+isValidA :: Password -> Bool
+isValidA (Password {letter, min, max, password}) = min <= numMatches && numMatches <= max
+  where
+    numMatches = T.foldr ((+) . boolToInt . (letter ==)) 0 password
+
+boolToInt :: Bool -> Int
+boolToInt b
+  | b = 1
+  | otherwise = 0
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = foldr ((+) . boolToInt . isValidB) 0
+
+isValidB :: Password -> Bool
+isValidB (Password {letter, min, max, password}) = minMatch /= maxMatch
+  where
+    checkIdx :: Int -> T.Text -> Bool
+    checkIdx i t = letter == T.index t (i-1)
+
+    minMatch = checkIdx min password
+    maxMatch = checkIdx max password
