d61292f6ce686bdc3afa438ced6ad087bfbd6077
Kelsey Freese <kmfreese@protonmail.com>
2020-12-05 00:53:52 -0600

    Day 5

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -2,18 +2,12 @@ module Days.Day05 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
 import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+
+import Control.Applicative (Alternative((<|>)))
 
 import qualified Program.RunDay as R (runDay)
-import Data.Attoparsec.Text
-import Data.Void
+import Data.Attoparsec.Text as P
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +15,62 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many' $ do
+  row <- count 7 $ char 'F' <|> char 'B'
+  col <- count 3 $ char 'L' <|> char 'R'
+  endOfLine
+  return $ SeatCode row col
+
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [SeatCode]
 
-type OutputA = Void
+data SeatCode = SeatCode { rowStr :: String
+                         , colStr :: String
+                         }
+                         deriving Show
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA codes = maximum $ map (rowColToSeatId . codeToRowCol) codes
+
+-- seat id is just arbitrarily defined in the problem
+rowColToSeatId :: (Int, Int) -> Int
+rowColToSeatId (r,c) = r * 8 + c
+
+codeToRowCol :: SeatCode -> (Int, Int)
+codeToRowCol SeatCode{..} = ( strToInt rowCharToBit rowStr
+                            , strToInt colCharToBit colStr
+                            )
+  where
+    -- seat codes are "binary space partitioned", where F/L means first half, B/R means second half.
+    -- Turns out this is identical to just using binary, since the number of rows & columns are both
+    -- powers of two
+    -- this fold just converts from "binary" to an Int by shifting the accumulated value left and
+    -- adding the new digit
+    strToInt :: (Char -> Int) -> String -> Int
+    strToInt f = foldl (\acc c -> acc * 2 + f c) 0
+
+    rowCharToBit 'F' = 0
+    rowCharToBit 'B' = 1
+
+    colCharToBit 'L' = 0
+    colCharToBit 'R' = 1
+
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB codes = fromJust $ findId sortedIds
+  where
+    -- look for two codes that are exactly 2 apart. result will be the number in between
+    findId :: [Int] -> Maybe Int
+    findId (x:xs@(y:_))
+      | abs (x - y) == 2 = Just $ (x + y) `div` 2
+      | otherwise        = findId xs
+    findId [] = Nothing
+
+    sortedIds = sort $ map (rowColToSeatId . codeToRowCol) codes
