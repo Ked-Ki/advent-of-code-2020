000866ad1a218002120997683e30022d64fe857b
Kelsey Freese <kmfreese@protonmail.com>
2020-12-08 23:03:27 -0600

    Day 8: use lenses

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -1,22 +1,21 @@
-module Days.Day08 (runDay) where
+{-# LANGUAGE TemplateHaskell #-}
+
+module Days.Day08 (runDay, exState) where
 
 {- ORMOLU_DISABLE -}
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Array
 
-import Data.Bifunctor (first, second)
 import Data.Functor (($>))
 
 import Control.Monad.State
+import Control.Lens
 
 import Data.Attoparsec.Text
 
 import qualified Program.RunDay as R (runDay)
 {- ORMOLU_ENABLE -}
-
-runDay :: Bool -> String -> IO ()
-runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
@@ -40,15 +39,24 @@ data Instruction = Nop Int
                  | Acc Int
                  deriving Show
 
-data SystemState = SystemState { acc :: Int
-                               , pc :: Int
+data SystemState = SystemState { _acc :: Int
+                               , _pc :: Int
                                }
                                deriving Show
+makeLenses ''SystemState
 
--- TODO use lens
-overAcc, overPC :: (Int -> Int) -> SystemState -> SystemState
-overAcc f ss = ss { acc = f (acc ss) }
-overPC f ss = ss { pc = f (pc ss) }
+data ExecutionState = ExecutionState { _sysState :: SystemState
+                                     , _visited :: Set Int
+                                     }
+                                     deriving Show
+makeLenses ''ExecutionState
+
+data BacktrackState = BacktrackState { _exState :: ExecutionState
+                                     , _modified :: Bool
+                                     }
+                                     deriving Show
+
+makeLenses ''BacktrackState
 
 type OutputA = SystemState
 
@@ -56,66 +64,47 @@ type OutputB = SystemState
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA ipt = fst $ execState (runTillLoop ipt) initState
+partA ipt = view sysState $ execState (runTillLoop ipt) initState
   where
-    initState = (SystemState 0 0, Set.empty)
+    initState = ExecutionState (SystemState 0 0) Set.empty
 
-runTillLoop :: Input -> State (SystemState, Set Int) ()
+runTillLoop :: Input -> State ExecutionState ()
 runTillLoop program = go
   where
     go = do
-      curPC <- gets (pc . fst)
-      visited <- gets snd
-      if Set.member curPC visited
+      curPC <- use (sysState.pc)
+      curVisited <- use visited
+      if Set.member curPC curVisited
          then return () -- hit a loop, bail out
          else let inst = program ! curPC
-              in modify (second $ Set.insert curPC) >>
+              in visited %= Set.insert curPC >>
                  runInst inst >>
                  go
 
     runInst inst = case inst of
-             Nop _ -> modify (first $ overPC (+1))
-             Acc i -> modify (first $ overAcc (+i) . overPC (+1))
-             Jmp i -> modify (first $ overPC (+i))
+             Nop _ -> sysState.pc += 1
+             Acc i -> sysState.acc += i >> sysState.pc += 1
+             Jmp i -> sysState.pc += i
 
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB ipt = sysState $ execState (backtrackOnLoop ipt) initState
+partB ipt = view (exState.sysState) $ execState (backtrackOnLoop ipt) initState
   where
-    initState = BacktrackState (SystemState 0 0) Set.empty False
-
--- TODO make TillLoopState w/o modified & have both implement overSysState & overVisited
--- this will allow sharing run* methods b/w both parts
--- ??? will this work with lenses w/o a typeclass?
-
-data BacktrackState = BacktrackState { sysState :: SystemState
-                                     , visited :: Set Int
-                                     , modified :: Bool
-                                     }
-                                     deriving Show
-
-overVisited :: (Set Int -> Set Int) -> BacktrackState -> BacktrackState
-overVisited f bs = bs { visited = f (visited bs) }
-
-overSysState :: (SystemState -> SystemState) -> BacktrackState -> BacktrackState
-overSysState f bs = bs { sysState = f (sysState bs) }
-
-setModified :: BacktrackState -> BacktrackState
-setModified bs = bs { modified = True }
+    initState = BacktrackState (ExecutionState (SystemState 0 0) Set.empty) False
 
 backtrackOnLoop :: Input -> State BacktrackState ()
 backtrackOnLoop program = go $> ()
   where
     go :: State BacktrackState Bool
     go = do
-      curPC <- gets (pc . sysState)
-      visited <- gets visited
-      if | Set.member curPC visited -> return False -- hit a loop, fail cur branch
+      curPC <- use $ exState.sysState.pc
+      curVisited <- use $ exState.visited
+      if | Set.member curPC curVisited -> return False -- hit a loop, fail cur branch
          | curPC > snd (bounds program) -> return True -- made it to the end of program, success
          | otherwise -> do -- run next instruction
              unwindState <- get
-             modify (overVisited (Set.insert curPC))
+             (exState.visited) %= Set.insert curPC
              let inst = program ! curPC
              case inst of
                Acc i -> runAcc i >> go
@@ -123,18 +112,23 @@ backtrackOnLoop program = go $> ()
                Nop i -> runNop >> go >>= unwindOrGo unwindState (runJmp i)
 
     runNop :: State BacktrackState ()
-    runNop = modify (overSysState (overPC (+1)))
+    runNop = (exState.sysState.pc) += 1
 
     runAcc, runJmp :: Int -> State BacktrackState ()
-    runAcc i = modify (overSysState (overAcc (+i) . overPC (+1)))
-    runJmp i = modify (overSysState (overPC (+i)))
+    runAcc i = (exState.sysState.pc) += 1 >> (exState.sysState.acc) += i
+    runJmp i = (exState.sysState.pc) += i
 
     unwindOrGo :: BacktrackState -> State BacktrackState () -> Bool -> State BacktrackState Bool
     unwindOrGo _ _ True = return True -- previous instruction led to termination, just return
     unwindOrGo unwoundState runModifiedInst False = 
-      put unwoundState >> gets modified >>= \isModified ->
+      put unwoundState >> use modified >>= \isModified ->
         if isModified
            -- we've already modified one instruction, this branch is dead
            then return False
            -- otherwise, modify current instruction and continue running
-           else modify setModified >> runModifiedInst >> go
+           else modified .= True >> runModifiedInst >> go
+
+-- Run day (moved to bottom to accommodate TH)
+runDay :: Bool -> String -> IO ()
+runDay = R.runDay inputParser partA partB
+
