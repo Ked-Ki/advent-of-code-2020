65baf081f5669238aca52ff010dd8f9af335b316
Kelsey Freese <kmfreese@protonmail.com>
2020-12-11 23:11:16 -0600

    Day 11: remove redundant imports

--- src/Days/Day11.hs
+++ src/Days/Day11.hs
@@ -2,21 +2,14 @@ module Days.Day11 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import qualified Data.List as L
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
 import qualified Util.Util as U
-import Util.Parsers (coordinateParser,Coordinates(..), prettyPrintCoords, overM)
+import Util.Parsers (coordinateParser,Coordinates(..), overM)
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
-import Debug.Trace (trace, traceShow)
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
