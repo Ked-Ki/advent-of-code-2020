11345a3fad7292656ddb2bf524ec06be65cedeef
Kelsey Freese <kmfreese@protonmail.com>
2020-12-12 16:40:25 -0600

    Day 12: tidy up

--- src/Days/Day12.hs
+++ src/Days/Day12.hs
@@ -60,12 +60,24 @@ data Boat = Boat { facing :: Direction
                  }
                  deriving Show
 
+overPos :: (Pos -> Pos) -> Boat -> Boat
+overPos f b@Boat{..} = b { pos = f pos }
+
+overFacing :: (Direction -> Direction) -> Boat -> Boat
+overFacing f b@Boat{..} = b { facing = f facing } 
+
 initBoat :: Boat
 initBoat = Boat East (0,0)
 
 data Waypoint = Waypoint { waypoint :: Pos
                          , boat :: Pos
                          }
+
+overWaypoint :: (Pos -> Pos) -> Waypoint -> Waypoint
+overWaypoint f w@Waypoint{..} = w { waypoint = f waypoint }
+
+overBoat :: (Pos -> Pos) -> Waypoint -> Waypoint
+overBoat f w@Waypoint{..} = w { boat = f boat }
 
 initWaypoint :: Waypoint
 initWaypoint = Waypoint (10,1) (0,0)
@@ -79,19 +91,21 @@ partA :: Input -> OutputA
 partA = pos . foldl runInstr initBoat
   where
     runInstr :: Boat -> Instruction -> Boat
-    runInstr b@Boat{..} (Cardinal North y) = b { pos = pos .+. (0,y)}
-    runInstr b@Boat{..} (Cardinal South y) = b { pos = pos .-. (0,y)}
-    runInstr b@Boat{..} (Cardinal East  x) = b { pos = pos .+. (x,0)}
-    runInstr b@Boat{..} (Cardinal West  x) = b { pos = pos .-. (x,0)}
-    runInstr b@Boat{..} TurnLeft = b { facing = l facing }
+    runInstr b (Cardinal North y) = overPos (.+. (0,y)) b 
+    runInstr b (Cardinal South y) = overPos (.-. (0,y)) b
+    runInstr b (Cardinal East  x) = overPos (.+. (x,0)) b
+    runInstr b (Cardinal West  x) = overPos (.-. (x,0)) b
+
+    runInstr b TurnLeft = overFacing l b
       where
         l = \case North -> West; West -> South; South -> East; East -> North
-    runInstr b@Boat{..} TurnAround = b { facing = a facing }
+    runInstr b TurnAround = overFacing a b
       where
         a = \case North -> South; West -> East; South -> North; East -> West
-    runInstr b@Boat{..} TurnRight = b { facing = r facing }
+    runInstr b TurnRight = overFacing r b
       where 
         r = \case North -> East; West -> North; South -> West; East -> South
+
     runInstr b@Boat{..} (Forward x) = runInstr b (Cardinal facing x)
 
 ------------ PART B ------------
@@ -99,14 +113,14 @@ partB :: Input -> OutputB
 partB = boat . foldl runInstr initWaypoint
   where
     runInstr :: Waypoint -> Instruction -> Waypoint
-    runInstr w@Waypoint{..} (Cardinal North y) = w { waypoint = waypoint .+. (0,y)}
-    runInstr w@Waypoint{..} (Cardinal South y) = w { waypoint = waypoint .-. (0,y)}
-    runInstr w@Waypoint{..} (Cardinal East  x) = w { waypoint = waypoint .+. (x,0)}
-    runInstr w@Waypoint{..} (Cardinal West  x) = w { waypoint = waypoint .-. (x,0)}
-    runInstr w@Waypoint{..} TurnLeft = w { waypoint = rotatePosL waypoint }
-    runInstr w@Waypoint{..} TurnAround = w { waypoint = (-1) .* waypoint }
-    runInstr w@Waypoint{..} TurnRight = w { waypoint = rotatePosR waypoint }
-    runInstr w@Waypoint{..} (Forward x) = w { boat = boat .+. (x .* waypoint) }
+    runInstr w              (Cardinal North y) = overWaypoint (.+. (0,y)) w
+    runInstr w              (Cardinal South y) = overWaypoint (.-. (0,y)) w
+    runInstr w              (Cardinal East  x) = overWaypoint (.+. (x,0)) w
+    runInstr w              (Cardinal West  x) = overWaypoint (.-. (x,0)) w
+    runInstr w               TurnLeft          = overWaypoint rotatePosL w
+    runInstr w               TurnAround        = overWaypoint ((-1) .*) w
+    runInstr w               TurnRight         = overWaypoint rotatePosR w
+    runInstr w@Waypoint{..} (Forward x)        = overBoat (.+. (x .* waypoint)) w
 
     rotatePosL :: Pos -> Pos
     rotatePosL (x,y) = (-y,x)
