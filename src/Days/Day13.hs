1f4b0afa2675c8ac0d3f2b41bc1153c71e6c1e94
Kelsey Freese <kmfreese@protonmail.com>
2020-12-14 05:21:00 -0600

    Day 13: comment/proof

--- src/Days/Day13.hs
+++ src/Days/Day13.hs
@@ -2,22 +2,13 @@ module Days.Day13 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
 import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
 import Control.Applicative.Combinators ((<|>))
-import Data.Ord (Down(..), comparing)
+import Data.Ord (comparing)
 import Data.Bifunctor (Bifunctor(second))
-import Debug.Trace (traceShow)
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -50,17 +41,38 @@ partA (dTime, ids) = second negate busIdWaitTime
 partB :: Input -> OutputB
 partB (_,bs) = go 0 allMbs
   where
-    -- turns out that a "pattern" of bus departures at time x for some bus ids [b1,b2..bn] next
-    -- reoccurs at exactly x + product(b1,b2..bn). While this was determined experimentally, it
-    -- makes sense since a) the input is all prime numbers and b) a pattern will reoccur for sure
-    -- at x + lcm(b1,b2..bn).
+    -- Let `b1,b2..bn` be the busses in the input, and let `m1,m2..mn` be the offset at which they
+    -- appear in the list. 
     --
-    -- This takes advantage of that fact and skips ahead by the product of all bus ids that matched
-    -- the desired "pattern" of departures on each loop.
-    go x mbs = let stride = product $ map snd $ filter (kiExists x) mbs
+    -- i.e. input of 17,x,13,19 has b1=17,m1=0; b2=13,m2=2; b3=19,m3=3
+    --
+    -- The goal of this part is to find a time `x` at which there exist `k1,k2..kn` s.t. 
+    -- `x + mi = ki * bi` for all `mi`,`bi`
+    --
+    -- Theorem: 
+    -- If there exists some `x` at which `x + mj = kj * bj` for all `j` in some subset `J`
+    -- of [1..n], then the next such `x` & `kj`s occur at `x + lcm(bj | j <- J)`. 
+    -- (i.e. if some time works for a couple busses, the next time those busses will work is lcm of
+    -- those busses away).
+    --
+    -- Proof:
+    -- Let `B = bj | j <- J`. at some time `y < x`, all busses in B depart simultaneously. 
+    -- `x - y` minutes later, busses `bj | j <- J` depart in the correct order. 
+    -- The next time all `b <- B` depart simultaneously is at `y + lcm(B)`, and thus the busses will
+    -- depart in the correct order `x - y` minutes after that, at `x + lcm(B)`. Moreover, there is
+    -- no other time `x'` where `x < x' < x + lcm(B)` at which this is true, because then at time
+    -- `x' - (x - y)` all busses will depart simultaneously, which would mean `x' - (x - y)` is a
+    -- multiple of all `bj <- B`, which contradicts the definition of least-common-multiple. QED
+    --
+    -- Thus, whenever we find an `x` that works for some set of busses, we can immediately skip
+    -- ahead by the lcm of that set of busses, and this will always find the smallest timestamp.
+    go x mbs = let stride = lcmList $ map snd $ filter (kiExists x) mbs
                 in if all (kiExists x) mbs
                       then x
                       else go (x + stride) mbs
+
+    lcmList :: [Integer] -> Integer
+    lcmList = foldr1 lcm
 
     kiExists :: Integer -> (Integer,Integer) -> Bool
     kiExists x (mi,bi) = (x + mi) `mod` bi == 0
