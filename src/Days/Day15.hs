4d677c8fd83d6f358339de88f2f761d1555f0c37
Kelsey Freese <kmfreese@protonmail.com>
2020-12-14 23:39:14 -0600

    Day 15 (brute force for part 2)

--- src/Days/Day15.hs
+++ src/Days/Day15.hs
@@ -1,19 +1,11 @@
 module Days.Day15 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +13,30 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = sepBy decimal ","
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA ipt = runGame ipt !! 2019
+
+runGame :: Input -> [Int]
+runGame ipt = ipt ++ go (length ipt + 1) (last ipt) (Map.fromList $ zip (init ipt) [1..])
+  where
+    go :: Int -> Int -> Map Int Int -> [Int]
+    go turn last m = case Map.lookup last m of
+                       Just x -> let newNum = turn - x - 1
+                                  in newNum : go (turn + 1) newNum newMap
+                       Nothing -> 0 : go (turn + 1) 0 newMap
+      where 
+        newMap = Map.insert last (turn - 1) m
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB ipt = runGame ipt !! 29999999
